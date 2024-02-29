b845bf4172accc7dbaa2abc08a978bb37610ff06
Dmitry Plotnikov <d.plotnikov@f5.com>
2024-02-29 10:06:53 -0800

    Use Abseil logging for Protobuf v22 and above (fix #16).
    (cherry picked from commit 93dc2b1878aa72d7c92dcb8fc8a08a198067b22d)

--- src/grpc_log.cpp
+++ src/grpc_log.cpp
@@ -3,6 +3,10 @@
 #include "grpc_log.hpp"
 
 #include <grpc/support/log.h>
+#include <google/protobuf/stubs/common.h>
+
+#if GOOGLE_PROTOBUF_VERSION < 4022000
+
 #include <google/protobuf/stubs/logging.h>
 
 class ProtobufLog {
@@ -25,6 +29,43 @@ private:
             msg.c_str());
     }
 };
+
+#else
+
+#include <absl/log/globals.h>
+#include <absl/log/initialize.h>
+#include <absl/log/log_sink_registry.h>
+
+class ProtobufLog : absl::LogSink {
+public:
+    ProtobufLog()
+    {
+        absl::InitializeLog();
+        absl::AddLogSink(this);
+        // Disable logging to stderr
+        absl::SetStderrThreshold(static_cast<absl::LogSeverity>(100));
+    }
+
+    ~ProtobufLog() override { absl::RemoveLogSink(this); }
+
+    void Send(const absl::LogEntry& entry) override
+    {
+        auto severity = entry.log_severity();
+
+        ngx_uint_t level =
+            severity == absl::LogSeverity::kFatal   ? NGX_LOG_EMERG :
+            severity == absl::LogSeverity::kError   ? NGX_LOG_ERR :
+            severity == absl::LogSeverity::kWarning ? NGX_LOG_WARN :
+                      /*absl::LogSeverity::kInfo*/    NGX_LOG_INFO;
+
+        ngx_str_t message { entry.text_message().size(),
+                            (u_char*)entry.text_message().data() };
+
+        ngx_log_error(level, ngx_cycle->log, 0, "OTel/protobuf: %V", &message);
+    }
+};
+
+#endif
 
 class GrpcLog {
 public:
